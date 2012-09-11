04e67dd1a8a931b7a1e18648fd6221842786ca0a
Ronnie Overby <ronnie@ronnieo.net>
2012-09-11 13:37:28 -0400

    init

---
+++ WpfApplication2/MainWindow.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System.Linq;
+
+namespace WpfApplication2
+{
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            listbox.ItemsSource = Enumerable.Range(1, 3);
+        }
+
+        private void ThisDrop(object sender, System.Windows.DragEventArgs e)
+        {
+            // do something
+        }
+
+        private void ThatDrop(object sender, System.Windows.DragEventArgs e)
+        {
+            // do something else
+        }
+    }
+}
