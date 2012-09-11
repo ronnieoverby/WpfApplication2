8a110b03541b08658ca8fed21b3cdff35700ffbb
Ronnie Overby <ronnie@ronnieo.net>
2012-09-11 15:07:49 -0400

    fixed issue

--- WpfApplication2/MainWindow.xaml.cs
+++ WpfApplication2/MainWindow.xaml.cs
@@ -1,4 +1,9 @@
-﻿using System.Linq;
+﻿using System;
+using System.Drawing;
+using System.Linq;
+using System.Windows;
+using System.Windows.Media.Imaging;
+using Image = System.Windows.Controls.Image;
 
 namespace WpfApplication2
 {
@@ -10,18 +15,18 @@ namespace WpfApplication2
             listbox.ItemsSource = Enumerable.Range(1, 3);
         }
 
-        /*
-         * THE ISSUE THAT I'M HAVING IS THAT THESE EVENTS DON'T FIRE WHEN A FILE IS DROPPED FROM WINDOWS EXPLORER!!!
-         */
-
-        private void ThisDrop(object sender, System.Windows.DragEventArgs e)
+        private void DropHandler(object sender, DragEventArgs e)
         {
-            // do something
-        }
-
-        private void ThatDrop(object sender, System.Windows.DragEventArgs e)
-        {
-            // do something else
+            var filePaths = e.Data.GetData(DataFormats.FileDrop) as string[];
+            var img = (Image) sender;
+            try
+            {
+                img.Source = new BitmapImage(new Uri(filePaths[0]));
+            }
+            catch
+            {
+                // probably not an image file
+            }
         }
     }
 }
